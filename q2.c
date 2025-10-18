e102da51bad82ebcc3ce969339a147cf9d0e98f6
mayafrncis <maya.ara.francis@gmail.com>
2025-10-18 09:15:57 +0000

    rearranged to original order

--- q2.c
+++ q2.c
@@ -3,20 +3,20 @@
 int main() {
 
 	struct Padding {
-		double c;
 		char a;
 		int b;
+		double c;
 	};
 	struct Padding s1;
 
 	printf("Size of struct: %ld\n", sizeof(s1));
-	printf("Size of double c field: %ld\n", sizeof(s1.c));
 	printf("Size of char a field: %ld\n", sizeof(s1.a));
 	printf("Size of int b field: %ld\n", sizeof(s1.b));
+	printf("Size of double c field: %ld\n", sizeof(s1.c));
 
-	printf("Address of double c field: %p\n", &s1.c);
 	printf("Address of char a field: %p\n", &s1.a);
 	printf("Address of int b field: %p\n", &s1.b);
-	
+	printf("Address of double c field: %p\n", &s1.c);
+
 	return 0;
 }
